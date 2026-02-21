985ce2954739a0bbe92e540b0d3d8a79e580763f
Wiscaksono <wwicaksono96@gmail.com>
2026-02-22 03:56:28 +0700

    chore: update dependencies and switch to vercel adapter

--- apps/homepage/svelte.config.js
+++ apps/homepage/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { mdsvex } from 'mdsvex';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
